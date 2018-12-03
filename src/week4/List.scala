dc42ff4a487083a79b47431ba28495dfe8fad18e
Ishan Bhatt <ishanbhatt@halwa>
2018-12-03 11:34:14 +0530

    Ended week3 with nth.sc implementation

--- src/week4/List.scala
+++ src/week4/List.scala
@@ -1,5 +1,7 @@
 package week4
 
+import java.util.NoSuchElementException
+
 trait List[T] {
   def isEmpty: Boolean
   def head: T
@@ -14,3 +16,20 @@ class Cons[T](val head: T,val tail: List[T]) extends List[T]{
   // That's the power of scala
   // val head: T is legal implementation of def head: T os List[T]
 }
+
+class Nil[T] extends List[T]{
+  override def isEmpty: Boolean = true
+
+  // Nothing is a subtype of any other type including T
+  override def head: Nothing = throw new NoSuchElementException
+
+  override def tail: Nothing= throw new NoSuchElementException
+
+}
+
+// Functions cn also have type parameters
+// def singleton[T](elem: T) = new Cons[T](elem, new Nil[T])
+// singleton[Int](1) OR singleton(1)
+// singleton[Boolean](true) OR singleton(true)
+// Type parameters are there at compile time only
+// You know it is type erasure
